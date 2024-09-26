0e8e9d32c4cf1dd23707f0a03f5bb71dfa16b923
ENvironmentSet <sbshw1@gmail.com>
2024-09-26 18:41:23 +0900

    implement custom input box

---
+++ components/LengthLimitedInput.tsx
@@ -0,0 +1,72 @@
+import { View, Text, TextInput, StyleProp, TextStyle, Pressable, Image, StyleSheet } from "react-native";
+
+interface LengthLimitedInputProps {
+  maxLength: number
+  value: string
+  onChangeText: (text: string) => void
+  placeholder?: string
+  style?: StyleProp<TextStyle>
+}
+
+function LengthLimitedInput({ maxLength, value, onChangeText, placeholder, style }: LengthLimitedInputProps) {
+  return (
+    <View style={[styles.container, style]}>
+      <TextInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        maxLength={maxLength}
+        style={styles.input}
+        placeholderTextColor="#C5CCD7"
+      />
+      <Text style={styles.count}>{value.length}/{maxLength}</Text>
+      {
+        value.length >= 1 &&
+          <Pressable onPress={() => onChangeText("")}>
+            <Image style={styles.clear} source={require("@/assets/images/x-circle-contained.png")} />
+          </Pressable>
+      }
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+
+    borderColor: "#DADFE5",
+    borderWidth: 1,
+    borderRadius: 12,
+    overflow: "hidden",
+  },
+  input: {
+    flexGrow: 1,
+    alignSelf: "stretch",
+
+    fontSize: 16,
+    fontWeight: 500,
+    
+    paddingVertical: 8,
+  },
+  count: {
+    flexShrink: 0,
+
+    color: "#A4A4A4",
+    fontSize: 10,
+    fontWeight: 400,
+    textAlign: "center",
+  },
+  clear: {
+    flexShrink: 0,
+    flexBasis: 20,
+
+    aspectRatio: 1,
+  },
+})
+
+export default LengthLimitedInput
