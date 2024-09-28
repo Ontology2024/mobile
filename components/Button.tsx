2aaaf51c924a423d1d4e74d2a191eb07085b94c2
ENvironmentSet <sbshw1@gmail.com>
2024-09-28 18:54:24 +0900

    implement 'active' prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,31 @@
 import { ReactNode } from "react"
-import { Pressable, StyleProp, ViewStyle } from "react-native"
+import { Pressable, StyleProp, ViewStyle, StyleSheet } from "react-native"
 
 interface ButtonProps {
   onPress: () => void
   children: ReactNode
+  active?: boolean
   style?: StyleProp<ViewStyle>
 }
 
-export default function Button({ onPress, children, style }: ButtonProps) {
+export default function Button({ onPress, children, active = true, style }: ButtonProps) {
   return (
-    <Pressable style={[{ backgroundColor: "#6028FF", paddingVertical: 14, borderRadius: 12, overflow: "hidden" }, style]} onPress={onPress}>
+    <Pressable style={[styles.base, ...active ? [] : [styles.inactive], style]} onPress={active ? onPress : () => {}}>
       {children}
     </Pressable>
   )
 }
+
+const styles = StyleSheet.create({
+  base: {
+    backgroundColor: "#6028FF",
+
+    paddingVertical: 14,
+
+    borderRadius: 12,
+    overflow: "hidden"
+  },
+  inactive: {
+    backgroundColor: "#F4F6F9"
+  }
+})
